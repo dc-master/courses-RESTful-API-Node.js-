102e38c0a613a1f545be6bc297e8b478c570388f
Amir Gorji <dc_master95@gmail.com>
2020-03-11 03:00:12 +0330

    add models to server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { mongoose } = require('./models/mongoose');
+const { User, Course } = require('./models/models');
 
 const app = express();
+app.use(express.json());
 
 app.get('/api/courses/', (req, res) => {
     res.send({ status: 'success' });
